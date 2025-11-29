764d779042a4805c5b395e886e5e379a804ddc42
Mahmoud <115042957+MahmoudNofaal@users.noreply.github.com>
2025-11-29 22:16:48 +0200

    Add Minimal API topics 2

--- Minimal API/MinimalAPI/obj/Debug/net9.0/MinimalAPI.AssemblyInfo.cs
+++ Minimal API/MinimalAPI/obj/Debug/net9.0/MinimalAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d5721fe0b2f8aeacc437d35841dd71c61c163f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
